0af86851c0b81a5cf83cc1d7fd227fe690673337
gabrielHawkins7 <74935796+gabrielHawkins7@users.noreply.github.com>
2024-03-23 16:02:54 -0400

    Simple Javalin setup

--- src/main/java/com/httpservertest/Main.java
+++ src/main/java/com/httpservertest/Main.java
@@ -1,12 +1,18 @@
 package com.httpservertest;
 
+
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinMustache;
 
 public class Main {
     public static void main(String[] args){
-        var app = Javalin.create().get("/", ctx -> ctx.result("Hello World"))
-                                    .start(7070);
-
+        var app = Javalin.create(
+            config ->{
+                config.fileRenderer(new JavalinMustache());
+            }
+        ).start(7070);
+        
+        app.get("/", ctx -> ctx.render("home.html"));
     }
 
 }
