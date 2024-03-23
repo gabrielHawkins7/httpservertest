e784b795bc99a3e4adaaabc6515674b327281416
gabrielHawkins7 <74935796+gabrielHawkins7@users.noreply.github.com>
2024-03-23 15:17:19 -0400

    Switched to Javalin

--- src/main/java/com/httpservertest/Main.java
+++ src/main/java/com/httpservertest/Main.java
@@ -1,25 +1,12 @@
 package com.httpservertest;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-
-import com.sun.net.httpserver.HttpServer;
+import io.javalin.Javalin;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
-        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
-        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor)Executors.newFixedThreadPool(10);
-        addRoutes(server);
-        server.setExecutor(threadPoolExecutor);
-        server.start();
-        System.out.println("Server Started on Port 8001");
+    public static void main(String[] args){
+        var app = Javalin.create().get("/", ctx -> ctx.result("Hello World"))
+                                    .start(7070);
+
     }
 
-    static void addRoutes(HttpServer server){
-        // server.createContext("/", new HomePage());
-        // server.createContext("/homeLogin", new HomeLogin());
-    }
 }
